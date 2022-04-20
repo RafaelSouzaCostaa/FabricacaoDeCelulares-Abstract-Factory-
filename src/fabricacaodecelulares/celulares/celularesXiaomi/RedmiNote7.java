84975c9cac5502dadbc2806a438832da5af1c74d
Rafael de Souza Costa <rafaeldesouzacosta.dev@gmail.com>
2022-04-20 19:05:24 -0300

    atualzação

---
+++ src/fabricacaodecelulares/celulares/celularesXiaomi/RedmiNote7.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fabricacaodecelulares.celulares.celularesXiaomi;
+
+import fabricacaodecelulares.celulares.Celular;
+
+/**
+ *
+ * @author Rafael de Souza Costa
+ */
+public class RedmiNote7 extends Celular {
+
+        public RedmiNote7() {
+                super("Redmi Note 7", "6GB", "128GB", "Xiaomi");
+        }
+}
